61f115c09198837aee15c9bbf09f77939d9aa52a
Pius <piusrestiantoro2@gmail.com>
2024-04-15 12:47:11 +0800

    fix: add product

--- src/pages/admin_dashboard/dashboard.jsx
+++ src/pages/admin_dashboard/dashboard.jsx
@@ -6,13 +6,14 @@ import NavbarComp from "../../components/navbar/navbar"
 import Api from "../../utils/api"
 import CardDashboard from "../../components/card/card.dashboard"
 import { Form, Modal } from "react-bootstrap"
-import { useNavigate } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import { useSelector } from "react-redux"
 
 function Dashboard() {
     const api = Api()
     const navigate = useNavigate()
     const {isAuth} = useSelector((state)=>state.users)
+    const params = useParams()
     const [category, setCategory] = useState([])
     const [product, setProduct] = useState([])
     const [newProduct, setNewProduct] = useState({
@@ -25,6 +26,10 @@ function Dashboard() {
     // TODO: fix add item
     const onChangeInput = (event)=>{
         event.preventDefault()
+
+        const tmpData = {...newProduct}
+        tmpData[event.target.name] = event.target.value
+        setNewProduct(tmpData)
     }
 
     const mainFunc = {
@@ -54,7 +59,18 @@ function Dashboard() {
             })
         },
 
-        // addProduct: async
+        addProduct: async ()=>{
+            await api.requests({
+                method: 'POST',
+                url: '/product/',
+                data: newProduct
+            }).then((res)=>{
+                alert('Product added successfully')
+                // navigate(`/dashboard/detail/${params.id}`)
+            }).catch((err)=>{
+                alert(err.message)
+            })
+        }
     }
 
     useEffect(()=>{
@@ -69,6 +85,7 @@ function Dashboard() {
             setAddItemModal(true)
         },
         saveData: ()=>{
+            mainFunc.addProduct()
             setAddItemModal(false)
         },
         cancel: ()=>{
@@ -105,21 +122,21 @@ function Dashboard() {
                                     </Modal.Header>
 
                                     <Modal.Body className="in-modal">
-                                        <p>Fix this form</p>
-                                        {/* <Form className="bg-transparent">
+                                        {/* <p>Fix this form</p> */}
+                                        <Form className="bg-transparent">
                                             <Form.Group className="mb-3 bg-transparent" controlId="formGroupText">
                                                 <Form.Label className="bg-transparent text-dark">Name</Form.Label>
-                                                <Form.Control type="text" name="name" placeholder="Product name" defaultValue={product.name} onChange={onChangeInput}/>
+                                                <Form.Control type="text" name="name" placeholder="Product name" onChange={onChangeInput}/>
                                             </Form.Group>
 
                                             <Form.Group className="mb-3 bg-transparent" controlId="formGroupText">
                                                 <Form.Label className="bg-transparent text-dark">Description</Form.Label>
-                                                <Form.Control as="textarea" name="description" rows={4} placeholder="Description" defaultValue={product.description} onChange={onChangeInput}/>
+                                                <Form.Control as="textarea" name="description" rows={4} placeholder="Description" onChange={onChangeInput}/>
                                             </Form.Group>
 
                                             <Form.Group className="mb-3 bg-transparent" controlId="formGroupText">
                                                 <Form.Label className="bg-transparent text-dark">Price</Form.Label>
-                                                <Form.Control type="text" name="price" placeholder="Price" defaultValue={product.price} onChange={onChangeInput}/>
+                                                <Form.Control type="text" name="price" placeholder="Price" onChange={onChangeInput}/>
                                             </Form.Group>
 
                                             <Form.Select aria-label="Default select example" name="category_id" value={product.category_id} onChange={onChangeInput}>
@@ -129,8 +146,18 @@ function Dashboard() {
                                                     )
                                                 })}
                                             </Form.Select>
-                                        </Form> */}
+                                        </Form>
                                     </Modal.Body>
+
+                                    <Modal.Footer className="in-modal">
+                                        <div className="d-flex" style={{background: 'none'}}>
+                                            <button className="logout-btn" onClick={addItemfunc.cancel}>Cancel</button>
+
+                                            <p style={{visibility: 'hidden'}}>m</p>
+
+                                            <button className="logout-btn" onClick={addItemfunc.saveData}>Save</button>
+                                        </div>
+                                    </Modal.Footer>
                                 </Modal>
                             </div>
 
