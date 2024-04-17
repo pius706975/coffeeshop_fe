41b653c6742f17e9c94302f8e3a5c4b763998e48
Pius <piusrestiantoro2@gmail.com>
2024-04-18 07:38:58 +0800

    fix: menu page. - implemented product filter to show products in each category>

--- src/pages/menu/menu.jsx
+++ src/pages/menu/menu.jsx
@@ -12,26 +12,62 @@ function Menu() {
     const api = Api()
 
     const [product, setProduct] = useState([])
+    // const [category, setCategory] = useState([])
 
-    const getAllProduct = async ()=>{
-        await api.requests({
-            method: 'GET',
-            url: '/product'
-        }).then((res)=>{
-            const data = res.data
-            setProduct(data)
-            // console.log(data);
-        }).catch((err)=>{
-            alert(err.message)
-        })
+    const mainFunc = {
+        getAllProducts: async ()=>{
+            await api.requests({
+                method: 'GET',
+                url: '/product/categorized'
+            }).then((res)=>{
+                const data = res.data
+                setProduct(data)
+                console.log(data);
+            }).catch((err)=>{
+                alert(err.message)
+            })
+        },
+
+        // getAllCategories: async ()=>{
+        //     await api.requests({
+        //         method: 'GET',
+        //         url: '/product-category/'
+        //     }).then((res)=>{
+        //         const data = res.data
+        //         setCategory(data)
+        //         console.log(data)
+        //     }).catch((err)=>{
+        //         alert(err.message)
+        //     })
+        // },
+
+        // TODO: fix this function
+        // getProductByCategory: async ()=>{
+        //     const allProduct = {}
+            
+        //     for (const cat of category ) {
+        //         await api.requests({
+        //             method: 'GET',
+        //             url: `/product/search?category=${cat}`
+        //         }).then((res)=>{
+        //             allProduct[cat.id] = res.data
+        //             setProduct(allProduct)
+        //             console.log(allProduct)
+        //         }).catch((err)=>{
+        //             alert(err.message)
+        //         })
+        //     }
+        // }
     }
 
     useEffect(()=>{
-        getAllProduct()
+        mainFunc.getAllProducts()
+        // mainFunc.getAllCategories()
+        // mainFunc.getProductByCategory()
         document.title = 'Product'
     }, [])
 
-    const displayProduct = product
+    // const displayProduct = product
     return (
         <div className="menu-app">
             <NavbarComp/>
@@ -56,25 +92,27 @@ function Menu() {
 
                         <p style={{visibility: 'hidden'}}>m</p>
 
-                        <h3 className="text-light">Menu</h3>
+                        <h3 className="text-light text-center fw-bold">Menu</h3>
 
                         <p style={{visibility: 'hidden'}}>m</p>
 
-                        <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'center', justifyItems: 'left', maxWidth: '1100px', marginBottom: '50px'}}>
-                            {
-                                displayProduct.map((data)=>{
-                                    return (
-                                        <div key={data.id}>
+                        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', maxWidth: '1100px', marginBottom: '50px' }}>
+                            {product.map(cat => (
+                                <div key={cat.categoryName}>
+                                    <h2 className="text-light fw-bold">{cat.categoryName}</h2>
+                                    <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+                                        {cat.products.map(prod => (
                                             <CardDetail
-                                                id={data.id}
-                                                name={data.name}
-                                                price={data.price}
-                                                image={data.image}
+                                                key={prod.id}
+                                                id={prod.id}
+                                                name={prod.name}
+                                                price={prod.price}
+                                                image={prod.image}
                                             />
-                                        </div>
-                                    )
-                                })
-                            }
+                                        ))}
+                                    </div>
+                                </div>
+                            ))}
                         </div>
                     </div>
                 </div>
